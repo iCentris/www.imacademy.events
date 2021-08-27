d14c8d06256c9d5dfc59b1aa6987331aff28256f
manuelruizc <manuelruizwebdev@gmail.com>
2021-08-27 13:50:12 -0500

    First commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2021, 10, 11, 23)}); // year, month, date, hour
+        });
+})(jQuery);
+
