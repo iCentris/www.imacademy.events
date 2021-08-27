3fbd26395d848b7031a46bee464aa6b495934a2a
manuelruizc <manuelruizwebdev@gmail.com>
2021-08-27 15:00:59 -0500

    changed countdown date

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,7 +1,7 @@
 (function($) {	
 	'use strict';
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2021, 10, 11, 23)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2021, 10, 4, 6)}); // year, month, date, hour
         });
 })(jQuery);
 
